4885ff5b97b5084ea40af409e8c526aa5024d20e
Delano <deltel000@gmail.com>
2021-05-05 15:58:40 -0600

    Update connection string

--- src/db/pg.js
+++ src/db/pg.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg')
 
-const connectionString = process.env.CONNECTION_STRING
+const connectionString = process.env.DATABASE_URL
 
 const client = new Client({
     connectionString
