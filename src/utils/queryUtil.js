6741e3e2495574bee5d6070c609540579117d667
Delano <deltel000@gmail.com>
2021-06-17 21:21:48 -0500

    Add edit pencil to details view

--- src/utils/queryUtil.js
+++ src/utils/queryUtil.js
@@ -1,19 +1,21 @@
 const getQueryValues = (dataObj) => {
-    let querySubstring = ''
-    let sep = ''
-    const updates = Object.keys(dataObj).concat('updated_at')
-    const updateValues = Object.values(dataObj).concat(new Date())
+  let querySubstring = "";
+  let sep = "";
+  const updates = Object.keys(dataObj).concat("updated_at");
+  const updateValues = Object.values(dataObj)
+    .map((value) => value.toLowerCase())
+    .concat(new Date());
 
-    updates.forEach((update, index) => {
-        const num = index + 1
-        if (index > 0) sep = ', '
-        querySubstring = querySubstring + sep + update + ' = ' + '$' + num
-    })
+  updates.forEach((update, index) => {
+    const num = index + 1;
+    if (index > 0) sep = ", ";
+    querySubstring = querySubstring + sep + update + " = " + "$" + num;
+  });
 
-    return {
-        querySubstring,
-        updateValues
-    }
-}
+  return {
+    querySubstring,
+    updateValues,
+  };
+};
 
-module.exports = getQueryValues
+module.exports = getQueryValues;
