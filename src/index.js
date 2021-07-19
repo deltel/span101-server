2c9f7c2832c40b85449f58fc2593db946d8ca567
Delano <deltel000@gmail.com>
2021-07-19 10:11:30 -0500

    Change default port number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const app = require('./app')
+const app = require("./app");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log('server listening on port', port)
-})
+  console.log("server listening on port", port);
+});
