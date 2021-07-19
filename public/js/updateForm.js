293fb63249d99e1d36cbe1080b7b7e5534b27ab2
Delano <deltel000@gmail.com>
2021-07-19 10:13:40 -0500

    Persist current word

--- public/js/updateForm.js
+++ public/js/updateForm.js
@@ -13,7 +13,6 @@ window.onload = async () => {
 
   const id = getId();
   const wordData = JSON.parse(localStorage.getItem("word"));
-  localStorage.removeItem("word");
 
   valueField.value = wordData.value;
   translationField.value = wordData.translation;
